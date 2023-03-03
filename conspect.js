4f0da1c48b719459de83798376dc9bf4397f6dcc
Maxim <benyachef@gmail.com>
2023-03-03 16:34:56 +0200

    Commit Home work Now

--- conspect.js
+++ conspect.js
@@ -228,80 +228,127 @@ for (let i = 0; i < 3; i++) {
 // *****
 // ******
 
-let result = "";
-const length = 7;
+// let result = "";
+// const length = 7;
 
-for (let i = 1; i < length; i++) {
+// for (let i = 1; i < length; i++) {
 
-    for ( let j = 0; j < i; j++){
-        result += "*";
-    }
+//     for ( let j = 0; j < i; j++){
+//         result += "*";
+//     }
 
-    result += "\n";
-}
+//     result += "\n";
+// }
 
-console.log(result);
+// console.log(result);
 
 
-first: for (let i = 0; i < 3; i++) {
-	console.log(`First level: ${i}`);
-	for (let j = 0; j < 3; j++) {
-		console.log(`Second level: ${j}`);
-        for (let k = 0; k < 3; k++) {
-            if (k === 2) continue first; // метка first и команда continue останавливает цикл и возвращает его к месту метки в нашем случае к верхнему циклц, метки нужно называть осознано так же как и переменные 
-            console.log(`Third level: ${k}`);
-        }
-	}
-}
+// first: for (let i = 0; i < 3; i++) {
+// 	console.log(`First level: ${i}`);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//             if (k === 2) continue first; // метка first и команда continue останавливает цикл и возвращает его к месту метки в нашем случае к верхнему циклц, метки нужно называть осознано так же как и переменные 
+//             console.log(`Third level: ${k}`);
+//         }
+// 	}
+// }
+
+// function firstTask() {
+//     for (let i = 5; i < 11; i++) {
+//         console.log(i)
+//     }
+    
+// }
+// firstTask();
+
+// function secondTask() {
+//     for ( let i = 20; i > 10; i--) {
+//         if (i === 13) {
+//             break;
+//         }
+//         console.log(i);
+//     }
+    
+// }
+
+// secondTask();
+
+// function thirdTask() {
+//     for (let i = 2; i <= 10; i++) {
+//         if ( i % 2 === 0) {
+//             console.log(i);
+//         }
+//     }
+    
+// }
+
+// thirdTask();
+
+// for (let i = 2; i <= 16; i++) {
+//         if (i % 2 === 0) {
+//             continue;
+//         } else {
+//             console.log(i);
+//         }
+//     }
+
+
+// let num = 2;
+
+// while (num <= 16) {
+//     if (num % 2 === 0) {
+//         num++;
+//         continue;
+//     } else {
+//         console.log(num);
+//     }
+//     num++;
+// } 
+
+// function fifthTask() {
+//     const arrayOfNumbers = [];
+//     for (let i = 5; i <= 10; i++) {
+//         arrayOfNumbers[i - 5] = i;
+//     }
+//     console.log(arrayOfNumbers);
+//     return arrayOfNumbers;
+    
+// }
+
+// fifthTask();
 
 function firstTask() {
-    for (let i = 5; i < 11; i++) {
-        console.log(i)
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
     }
     
+    
+    console.log(result);
+    return result;
 }
+
 firstTask();
 
 function secondTask() {
-    for ( let i = 20; i > 10; i--) {
-        if (i === 13) {
-            break;
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, "Shopping", 20, "Homework"];
+
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === "number") {
+           data[i] = data[i] * 2;
+        } else if (typeof(data[i]) === "string") {
+            data[i] = `${data[i]} - done`;
         }
-        console.log(i);
     }
     
+    
+    console.log(data);
+    return data;
 }
 
 secondTask();
-
-function thirdTask() {
-    for (let i = 2; i <= 10; i++) {
-        if ( i % 2 === 0) {
-            console.log(i);
-        }
-    }
-    
-}
-
-thirdTask();
-
-for (let i = 2; i <= 16; i++) {
-        if (i % 2 === 0) {
-            continue;
-        } else {
-            console.log(i);
-        }
-    }
-
-
-let num = 2;
-
-while (num <= 16) {
-    if (num % 2 === 0) {
-        num++;
-        continue;
-    } else {
-        console.log(num);
-    }
-    num++;
-} 
