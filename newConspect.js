6174885ca961d5f8c385d26f6b1413ff045a42bf
Maxim <benyachef@gmail.com>
2023-03-06 20:27:28 +0200

    next home work, object next

--- newConspect.js
+++ newConspect.js
@@ -234,20 +234,44 @@ const personalPlanPeter = {
         languages: ["ru", "eng"],
         programmingLangs: {
             js: "20%",
-            php: "10%"
+            php: "10%",
+            ruby: "30%"
         },
         exp: "1 month"
+    },
+    showAgeAndLangs: function(plan) {
+        const {age} = plan;
+        const {languages} = plan.skills;
+        let str = `Мне ${age} лет и я владею языка:`;
+
+        languages.forEach(function(lang) {
+            str += `${lang.toUpperCase()} `;
+        });
+        
+        console.log(str);
+        return str;
+        
     }
 };
 
-function showExperience(plan) {
-    let {name, age, skills:{languages, programmingLangs, exp} } = plan;
-    console.log(exp);
-    return exp;
-}
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
+// function showExperience(plan) {
+//     let {exp} = plan.skills;
+//     console.log(exp);
+//     return exp;
+// }
 
-showExperience(personalPlanPeter);
+// showExperience(personalPlanPeter);
 
 function showProgrammingLangs(plan) {
-
+    let str = "";
+    const {programmingLangs} = plan.skills;
+    for (let key in programmingLangs) {
+        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`; // оператор += добовляет и присваевает к переменной результат работы цикла, тоесть сначало цикл добавляет результат первой пробежки, а потом присваевает результат второй пробежки, но при этом старый результат сохраняется 
+    }
+    console.log(str);
+    return str;
 }
+
+
+showProgrammingLangs(personalPlanPeter);
